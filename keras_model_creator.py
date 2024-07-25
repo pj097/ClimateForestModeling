619428d9572b2fd84c3bc6fe56e2590729b4d26a
Your Name <you@example.com>
2024-07-25 18:16:10 +0100

    added report, modified .gitignore to be recursive with data types

---
+++ keras_model_creator.py
@@ -0,0 +1,241 @@
+from IPython.display import display, HTML
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
+
+from tensorflow.keras.layers import *
+
+from sklearn.model_selection import train_test_split
+
+import pandas as pd
+import numpy as np
+
+import data_generator
+from importlib import reload
+reload(data_generator)
+from data_generator import DataGenerator
+
+def brier_score(y_true, y_pred):
+    brier_score = tf.reduce_mean((y_pred - y_true) ** 2, axis=1)
+    return brier_score
+
+class KerasModelCreator:
+    def __init__(self, **kwargs):
+        vars(self).update(kwargs)
+        self.kwargs = kwargs
+            
+    def display_logger(self, log_file, metrics):
+        metric_names = [m if isinstance(m, str) else m.name for m in metrics]
+        
+        if not self.verbose:
+            return
+            
+        if log_file.is_file() and log_file.stat().st_size > 0:
+            sort_key = lambda x: x.split('_')[-1]
+            val_metrics = ['val_loss'] + ['val_' + x for x in metric_names]
+            all_metrics = ['loss'] + metric_names + val_metrics
+            df = pd.read_csv(log_file)[['epoch'] + sorted(all_metrics, key=sort_key)]
+            df['epoch'] += 1
+            df = df.astype(str)
+            df.loc[df.shape[0]] = df.columns
+            print('Previous training:')
+            display(HTML(df.to_html(index=False)))
+    
+    def define_callbacks_and_logger(self, log_file, metrics):
+        metric_names = [m if isinstance(m, str) else m.name for m in metrics]
+
+        callbacks = [
+            tf.keras.callbacks.BackupAndRestore(
+                self.model_dir, save_freq='epoch', delete_checkpoint=False
+            ),
+            tf.keras.callbacks.CSVLogger(log_file, append=True),
+            tf.keras.callbacks.ModelCheckpoint(
+                self.model_dir.joinpath('model.keras'), 
+                monitor='val_recall', save_best_only=True, 
+                save_freq='epoch', initial_value_threshold=0.1,
+                verbose=0,
+            ),
+            tf.keras.callbacks.ReduceLROnPlateau(
+                monitor='val_recall', factor=0.5, patience=4, min_lr=1e-7,
+                verbose=self.verbose,
+            ),
+            tf.keras.callbacks.EarlyStopping(
+                monitor='val_recall', 
+                verbose=self.verbose,
+                patience=20,
+                mode='max',
+                restore_best_weights=True
+            ),
+            tf.keras.callbacks.TensorBoard(
+                log_dir=self.model_dir.joinpath('tensorboard_logs'),
+                histogram_freq=0,
+                write_graph=True,
+                update_freq='epoch',
+                embeddings_freq=0,
+            )
+        ]
+        return callbacks
+
+    def get_metrics(self):
+        prc = tf.keras.metrics.AUC(name='prc', curve='PR')
+
+        f1_scores = []
+        for average in ['micro', 'macro', 'weighted']:
+            f1_scores.append(
+                tf.keras.metrics.F1Score(
+                    average=average, threshold=0.5, name=f'{average}f1score')
+            )
+        metrics = [
+            'accuracy', 'recall', 'precision', 'auc', prc
+        ] + f1_scores
+
+        return metrics
+        
+    def run(self):
+        ''' 
+        If not overwrite and there's an existing model, the model will 
+        continue training if the given epoch is bigger than the previous,
+        else just evaluate.
+        Ensure train splits are the same across continuations / evaluations
+        by not modifying the random_state in split_and_normalise.
+        '''
+        log_file = self.model_dir.joinpath('model.log')
+        
+        metrics = self.get_metrics()
+
+        if self.overwrite:
+            for f in self.model_dir.rglob('*'):
+                if f.is_file(): f.unlink()
+                
+        self.display_logger(log_file, metrics)
+        
+        callbacks = self.define_callbacks_and_logger(log_file, metrics)
+
+        shard_ids = self.utils.selected_classes.index
+        training_ids, test_ids = train_test_split(shard_ids, test_size=10000, random_state=42)
+
+        training_generator = DataGenerator(training_ids, shuffle=True, **self.kwargs)
+        testing_generator = DataGenerator(test_ids, shuffle=False, **self.kwargs)
+    
+        if self.model_dir.joinpath('model.keras').is_file():
+            model = tf.keras.models.load_model(self.model_dir.joinpath('model.keras'))
+        else:
+            model = self.build_model(
+                self.utils.selected_classes.shape[1], metrics, self.loss,
+                output_bias=self.utils.data_summary['initial_bias'],
+            )
+        model.fit(
+            x=training_generator,
+            validation_data=testing_generator,
+            epochs=self.epochs,
+            callbacks=callbacks,
+            class_weight=self.utils.data_summary['class_weights'],
+            verbose=self.verbose
+        )
+        return model, testing_generator
+
+    def soil_layers(self, x):
+        for filters_scale in [2, 4, 8, 16]:
+            x = Conv2D(
+                filters=self.base_filters*filters_scale, 
+                kernel_size=3, padding='same', activation='relu',
+            )(x)
+            x = MaxPooling2D(pool_size=2, strides=2, padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Dropout(self.dropout)(x)
+        
+        x = Flatten()(x)
+        
+        x = Dense(self.base_filters*2, activation='relu')(x)
+        x = BatchNormalization()(x)
+        x = Dropout(self.dropout*2)(x)
+        return x
+
+    def elevation_layers(self, x):
+        for filters_scale in [2, 4, 8, 16]:
+            x = Conv2D(
+                filters=self.base_filters*filters_scale,
+                kernel_size=3, padding='same', activation='relu',
+            )(x)
+            x = MaxPooling2D(pool_size=2, strides=2, padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Dropout(self.dropout)(x)
+        
+        x = Flatten()(x)
+        
+        x = Dense(self.base_filters, activation='relu')(x)
+        x = BatchNormalization()(x)
+        x = Dropout(self.dropout*2)(x)
+        return x
+
+    def sentinel_layers(self, x):
+        for filters_scale in [2, 4, 8, 16]:
+            # x = ConvLSTM2D(
+            #     filters=self.base_filters*filters_scale, kernel_size=3, padding='same',
+            #     return_sequences=True
+            # )(x)
+            x = Conv3D(
+                filters=self.base_filters*filters_scale, 
+                kernel_size=3, padding='same',
+                activation='relu',
+            )(x)
+            x = MaxPooling3D(pool_size=2, strides=2, padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Dropout(self.dropout)(x)
+
+        x = Conv3D(
+            filters=self.base_filters*32, 
+            kernel_size=3, padding='same',
+            activation='relu',
+        )(x)
+            
+        x = Flatten()(x)
+
+        x = Dense(self.base_filters*8, activation='relu')(x)
+        x = BatchNormalization()(x)
+        x = Dropout(self.dropout)(x)
+        return x
+
+    def build_model(self, output_shape, metrics, loss, output_bias=None):
+        if output_bias is not None:
+            output_bias = tf.keras.initializers.Constant(output_bias)
+
+        sentinel_input = Input((len(self.seasons), 100, 100, 
+                                len([b for b in self.bands if b < 10])))
+       
+        x = self.sentinel_layers(sentinel_input)
+        
+        inputs = [sentinel_input]
+        
+        if 10 in self.bands:
+            elevation_input = Input((100, 100, 1))
+            inputs += [elevation_input]
+            x = concatenate([x, self.elevation_layers(elevation_input)])
+
+        n_soil_bands = len([b for b in self.bands if b > 10])
+        if n_soil_bands:
+            soil_input = Input((4, 4, n_soil_bands))
+            inputs += [soil_input]
+            x = concatenate([x, self.soil_layers(soil_input)])
+
+        for units_scale in [32, 16, 8, 4]:
+            x = Dense(self.base_filters*units_scale, activation='relu')(x)
+            x = Dropout(self.dropout)(x)
+        
+        outputs = Dense(output_shape, activation='sigmoid', bias_initializer=output_bias)(x)
+
+        m = tf.keras.models.Model(inputs=inputs, outputs=outputs)
+
+        opt = tf.keras.optimizers.Adam(
+            learning_rate=1e-4,
+            beta_1=0.9,
+            beta_2=0.999,
+        )
+
+        m.compile(optimizer=opt, loss=loss, metrics=metrics)
+        
+        return m
+
+
---
+++ sentinel_utils.py
@@ -0,0 +1,116 @@
+import json
+from collections import OrderedDict
+from pathlib import Path
+from tqdm.notebook import tqdm
+import numpy as np
+import pandas as pd
+from sklearn.utils import shuffle
+
+from concurrent.futures import ThreadPoolExecutor
+
+class SentinelUtils:
+    def __init__(self, seasons, shards_dir, all_bands, sample_shards=10000,
+                 min_occurrences=0, overwrite_existing=False):
+        # sentinel_bands = [f'B{x}' for x in range(2, 9)] + ['B8A', 'B11', 'B12']
+        # soilgrids_band = ['bdod', 'cec', 'cfvo', 'clay', 'nitrogen', 'ocd',
+        #                   'ocs', 'phh2o', 'sand', 'silt', 'soc']
+        # all_bands = sentinel_bands + ['Elevation'] + soilgrids_band
+        tmp = Path('tmp')
+        tmp.mkdir(exist_ok=True)
+
+        sentinel_shards = []
+        for s in seasons:
+            path_list = list(shards_dir.joinpath(f'features_2017{s}').glob('feature_*.npy'))
+            sentinel_shards.extend(path_list)
+        sentinel_shards = shuffle(sentinel_shards, random_state=42)[:sample_shards]
+        
+        summary_path = tmp.joinpath(f'data_summary_{"_".join(seasons)}_{min_occurrences}.json')
+        selected_classes_path = tmp.joinpath(f'selected_classes_{min_occurrences}.csv')
+        
+        if summary_path.is_file() and not overwrite_existing:
+            self.data_summary = json.loads(summary_path.read_text())
+            self.selected_classes = pd.read_csv(selected_classes_path, index_col='selected_index')
+        else:
+            all_labels = pd.read_csv(Path('data').joinpath('full_dummies.csv'))
+            data_summary = self.process_features(all_bands, sentinel_shards)
+            self.selected_classes, self.data_summary = self.process_labels(
+                all_labels, min_occurrences, data_summary, summary_path, selected_classes_path
+            )
+
+    def process_features(self, all_bands, sentinel_shards):
+        data_summary = {}
+        for stat in ['std', 'mean']:
+            data_summary[stat] = OrderedDict()
+
+        shards_dir = sentinel_shards[0].parent.parent
+        elevation_shards = list(shards_dir.joinpath('elevations').glob('elevation_*.npy'))
+        elevation_shards = shuffle(elevation_shards, random_state=42)[:len(sentinel_shards)]
+        soil_shards = list(shards_dir.joinpath('soil').glob('soil_*.npy'))
+        soil_shards = shuffle(soil_shards, random_state=42)[:len(sentinel_shards)]
+
+        for i, band in enumerate(tqdm(all_bands, leave=False)):
+            band_data = []
+            if i < 10:
+                for shard_path in (pbar := tqdm(sentinel_shards, leave=False)):
+                    pbar.set_description(band)
+                    with open(shard_path, 'rb') as f:
+                        data = np.load(f)
+                        band_data.append(np.copy(data[..., i]))
+            elif i == 10:
+                for shard_path in (pbar := tqdm(elevation_shards, leave=False)):
+                    pbar.set_description(f'Soil {band}')
+                    with open(shard_path, 'rb') as f:
+                        data = np.load(f)
+                        band_data.append(np.copy(data))
+            else:
+                for shard_path in (pbar := tqdm(soil_shards, leave=False)):
+                    pbar.set_description(band)
+                    with open(shard_path, 'rb') as f:
+                        data = np.load(f)
+                        band_data.append(np.copy(data[..., i - 11]))
+
+            band_data = np.vstack(band_data)
+            data_summary['mean'][band] = band_data.mean()
+            data_summary['std'][band] = band_data.std()
+        
+        return data_summary
+
+    def process_labels(self, all_labels, min_occurrences, data_summary, 
+                       summary_path, selected_classes_path):
+        
+        grouped_labels = all_labels.T.groupby(
+            all_labels.columns.str.split().str[0],
+        ).max().T
+
+        keep_classes = np.where(grouped_labels.sum(axis=0) >= min_occurrences)[0]
+        selected_labels = grouped_labels.iloc[:, keep_classes]
+
+        keep_shards = np.where(selected_labels.sum(axis=1) > 0)[0]
+        
+        selected_labels = selected_labels.iloc[keep_shards, :]
+    
+        neg, pos = np.bincount(selected_labels.to_numpy().astype(int).flatten())
+ 
+        initial_bias = np.log([pos/neg])
+        data_summary['initial_bias'] = initial_bias[0]
+        
+        class_weights = selected_labels.shape[0]/(selected_labels.shape[1]*selected_labels.sum(axis=0))
+        class_indices = list(range(selected_labels.shape[1]))
+        class_weights = dict(zip(class_indices, class_weights.tolist()))
+        data_summary['class_weights'] = class_weights
+
+        selected_labels.to_csv(selected_classes_path, index_label='selected_index')
+        summary_path.write_text(json.dumps(data_summary))
+        
+        return selected_labels, data_summary
+    
+    def normalise(self, X, bands, outliar_threshold=3):
+        stats = {}
+        for stat in ['mean', 'std']:
+            stats[stat] = np.array(list(self.data_summary[stat].values()))
+            
+        normalised_X = (X - stats['mean'][bands])/stats['std'][bands]
+        return normalised_X
+    
+
+    
